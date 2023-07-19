b600def9843372dc95ac12c7930f1a881e7f1b2a
SebasOso <criptodesc09@gmail.com>
2023-07-19 07:29:24 -0500

    OndaDeVientooo

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private GameObject particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Holi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
