d49da08c3f4b04ea6b068afe8adb0006720a0d0c
Sebastian Osorio <83346917+SebasOso@users.noreply.github.com>
2023-09-09 14:44:05 -0500

    FinalLaser

--- Assets/Ejercicios/Ejercicio2/SuperPunch.cs
+++ Assets/Ejercicios/Ejercicio2/SuperPunch.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class SuperPunch : MonoBehaviour
 {
     [SerializeField] private ParticleSystem superPunch;
-    
+    [SerializeField] private VisualEffect finalLaser;
     public void PlayPunch()
     {
         superPunch.Play();
+    }
+    public void PlayLaser()
+    {
+        finalLaser.Play();
     }
 }
