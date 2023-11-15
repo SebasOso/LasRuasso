705554f16223e0f76ba4b4086221e3946ac58b3d
Sebastian Osorio <83346917+SebasOso@users.noreply.github.com>
2023-11-15 10:27:11 -0500

    DoneTornado

--- Assets/Ejercicios/EjercicioFinalDemo/Scripts/AbilitiesManager.cs
+++ Assets/Ejercicios/EjercicioFinalDemo/Scripts/AbilitiesManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class AbilitiesManager : MonoBehaviour
 {
+    [SerializeField] private Material gaussMat;
     [SerializeField] private GameObject tornadoPrefab;
     private InputReader inputReader;
     [SerializeField] private Transform tornadoSpawnPosition;
@@ -23,13 +24,39 @@ public class AbilitiesManager : MonoBehaviour
     {
         print("TORNADO");
         GameObject tornado = Instantiate(tornadoPrefab, tornadoSpawnPosition.position, Quaternion.Euler(new Vector3(0,0,0)));
-        
+        StartCoroutine(IncreaseGauss());
         StartCoroutine(Tornado(tornado));
     }
     private IEnumerator Tornado(GameObject tornado)
     {
         yield return new WaitForSeconds(9f);
+        StartCoroutine(DecreaseGauss());
         inputReader.IsCasting = false;
         Destroy(tornado);
+    }
+    private IEnumerator IncreaseGauss()
+    {
+        float initialValue = 0f;
+        float targetValue = 9f;
+        int steps = 1000;
+        float stepValue = (targetValue - initialValue) / steps;
+        for (int i = 0; i <= steps; i++)
+        {
+            gaussMat.SetFloat("_PixelOffSet", initialValue + i * stepValue);
+            yield return null;
+        }
+    }
+    private IEnumerator DecreaseGauss()
+    {
+        float initialValue = 9f;
+        float targetValue = 0f;
+        int steps = 1000;
+        float stepValue = (targetValue - initialValue) / steps;
+        for (int i = 0; i <= steps; i++)
+        {
+            gaussMat.SetFloat("_PixelOffSet", initialValue + i * stepValue);
+            yield return null;
+        }
+        gaussMat.SetFloat("_PixelOffSet", 0f);
     }
 }
